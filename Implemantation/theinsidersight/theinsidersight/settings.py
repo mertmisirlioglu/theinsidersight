5b6eef004b7fd32dc3851337b2cf3ea70e759d5e
Mert Mısırlıoğlu <mert.misirlioglu@isik.edu.tr>
2020-01-26 04:47:15 +0300

    for publish.

--- Implemantation/theinsidersight/theinsidersight/settings.py
+++ Implemantation/theinsidersight/theinsidersight/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'theinsidersight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insider_sight',
+        'USER': 'insider',
+        'PASSWORD':'mert4199',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
