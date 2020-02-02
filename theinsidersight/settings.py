775b83516a03d995b190ed8b299615710d3bfd0e
Mert Mısırlıoğlu <mert.misirlioglu@isik.edu.tr>
2020-02-02 19:21:30 +0300

    some changes

--- theinsidersight/settings.py
+++ theinsidersight/settings.py
@@ -127,9 +127,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'theinsidersight@gmail.com'
-EMAIL_HOST_PASSWORD = 'qjgpucyyjwfvovui'
-EMAIL_PORT = 587
